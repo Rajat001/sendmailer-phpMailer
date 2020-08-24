0d5e45b5c63d05e761ef503a8132734c64903118
Rajat001 <singhrajat869@gmail.com>
2020-08-24 15:35:46 +0530

    SendMail Using PhpMailer From LocalServer

---
+++ projectone/index.php
@@ -0,0 +1,99 @@
+<?php
+
+if(isset($_POST['sendmail'])){
+require 'PHPMailerAutoload.php';
+require 'credentials.php';
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = EMAIL;                 // SMTP username
+$mail->Password = PASS;                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom(EMAIL, 'Rajat Singh Mailer Check');
+$mail->addAddress($_POST['email']);     // Add a recipient
+
+$mail->addReplyTo(EMAIL);
+
+// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+}
+?>
+
+<html>
+<head>
+<title>User Login</title>
+<style>
+body{
+	font-family: calibri;
+}
+.tblLogin {
+	border: #95bee6 1px solid;
+    background: #d1e8ff;
+    border-radius: 4px;
+    max-width: 300px;
+	padding:20px 30px 30px;
+	text-align:center;
+}
+.tableheader { font-size: 20px; }
+.tablerow { padding:20px; }
+.error_message {
+	color: #b12d2d;
+    background: #ffb5b5;
+    border: #c76969 1px solid;
+}
+.message {
+	width: 100%;
+    max-width: 300px;
+    padding: 10px 30px;
+    border-radius: 4px;
+    margin-bottom: 5px;    
+}
+.login-input {
+	border: #CCC 1px solid;
+    padding: 10px 20px;
+	border-radius:4px;
+}
+.btnSubmit {
+	padding: 10px 20px;
+    background: #2c7ac5;
+    border: #d1e8ff 1px solid;
+    color: #FFF;
+	border-radius:4px;
+}
+</style>
+</head>
+
+	<body>
+
+	<form name="frmUser" method="post" action="">
+
+	<div class="tblLogin">
+	<div class="tableheader">Enter Your Login Email</div>
+	<div class="tablerow"><input type="text" name="email" placeholder="Email" class="login-input" required></div>
+	<div class="tableheader"><input type="submit" name="sendmail" value="SendMail" class="btnSubmit"></div>
+	</div>
+
+	</form>
+
+	</body>
+	</html>
---
+++ projectone/readme.txt
@@ -0,0 +1,7 @@
+For Running this Project we have to Access Less Secured App To Be ON Mode for sender Mailer Id In GMAIL Account , now in this Case singhrajat00777@gmail.com is Gmail Id , After On the Mode , the mail will be send to the user ...
+
+
+Below Is YOUTube Video For Running and Making this Sendmailer/ProjectOne
+
+https://www.youtube.com/watch?v=37tSrPc983s
+https://www.youtube.com/watch?v=mQT-94ok0rs
